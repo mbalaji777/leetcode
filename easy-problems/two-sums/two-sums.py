62c87b19fc3f263683341d7d5e95be3958aa9a22
mbalaji777 <mbalaji.manikandan@gmail.com>
2022-10-12 17:51:11 -0400

    Implemented 1.twoSums

--- easy-problems/two-sums/two-sums.py
+++ easy-problems/two-sums/two-sums.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 def twoSum(nums, target):
     """
     :type nums: List[int]
@@ -7,12 +10,15 @@ def twoSum(nums, target):
     indexer = 1
     for element in nums:
         if (target-element) in nums[indexer:]:
-            return [nums.index(element), nums.index(target-element)]
+            if nums.count(target-element)> 1 : 
+                return [nums.index(target-element),nums.index(element, (nums.index(element) + 1))]
+            else : 
+                return [nums.index(element), nums.index(target-element)]
         indexer+=1
     return -1
 
 if __name__ == "__main__":
-    nums = [3,3]
-    target = 6
+    nums = [2,7,11,5]
+    target = 9
     x = twoSum(nums, target)
     print(x)
