835788a7f5e13014ced61764196f3fa03807f3b3
mbalaji777 <mbalaji.manikandan@gmail.com>
2022-10-12 16:38:11 -0400

    Binary Search Initial Code

---
+++ binary-search/binary-search.py
@@ -0,0 +1,24 @@
+from math import floor
+import math
+def search(nums: int, target: int):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    mid = math.floor(len(nums) / 2)
+    
+    if nums[mid] == target:
+        return mid
+    elif nums[mid] > target:
+        return search(nums[0:mid], target)
+    elif nums[mid] < target:
+        return search(nums[mid:len(nums)], target)
+    
+
+if __name__ == "__main__":
+    print("Hello")
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 9
+    x = search(nums, target)
+    print(x)
